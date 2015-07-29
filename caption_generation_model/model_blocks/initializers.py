8b7f826bf3275862433a6a52eadc86d9144b9e87
sergii-gavrylov <sergii.gavrylov@grammarly.com>
2015-07-29 23:37:31 +0300

    simplify initializers

--- caption_generation_model/model_blocks/initializers.py
+++ caption_generation_model/model_blocks/initializers.py
@@ -31,9 +31,8 @@ class Orthogonal(object):
         self.shape = shape
 
     def __call__(self):
-        flat_shape = (self.shape[0], np.prod(self.shape[1]))
-        a = r.normal(0.0, 1.0, flat_shape)
+        a = r.normal(0.0, 1.0, self.shape)
         u, _, v = np.linalg.svd(a, full_matrices=False)
-        q = u if u.shape == flat_shape else v
+        q = u if u.shape == self.shape else v
         q = q.reshape(self.shape).astype(np.float32)
         return q
